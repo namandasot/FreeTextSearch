01ed9f151b815b0bb31f2751811d455cb5f125dd
Prasoon Kumar <prasoon.kumar@hdfcred.com>
2016-11-09 15:48:24 +0530

    updated mobile api

--- api_final.py
+++ api_final.py
@@ -1,6 +1,8 @@
 from flask import Flask,request,jsonify
 from NLP import *
 from GooglePlaceDetailAPI import *
+from urllib import urlopen
+import json
 import requests
 import urllib2
 from gevent.wsgi import WSGIServer
@@ -13,18 +15,11 @@ import time
 @app.route('/')
 def get():
     starttime = time.time()
-    preference=[]
-    preference_dict={}
     todo_id=request.args['searchstring']
-    try:
-        token_id = request.headers.get('token_id')
-        if(token_id == None):
-            token_id= '12345'
-    except:
-        token_id = '12345'
-    print "token_id",token_id
-    print "todo_id ",todo_id
+    #print "todo_id ",todo_id
     [query,bhk,bhk_desc,apt_type,budget,budget_item,budget_desc,amenities,location,possession,possession_desc,date]=start(todo_id)
+    total_budget=0
+    print "++++++++++++++++++++++++++++++++++++"
     total_budget=0
     if budget_desc:
         for item in budget_desc:
@@ -48,7 +43,7 @@ def get():
                 if item in ['lac','l','lakh','lacs','lakhs']:
                     total_budget=total_budget*100000
                     break
-        preference_dict['budget']=total_budget
+
         
     poss=0
     if possession:
@@ -82,11 +77,8 @@ def get():
                 if  possession >=36:
                     poss=5
 
-
-            preference_dict['possession']=poss
-
-
-    string = "https://hdfcred.com/mobile_v3/project_listing_new_revised/?"
+    
+    string="https://hdfcred.com/mobile_v3/project_listing_new_revised/?"
     str1=string
     lat=[]
     log=[]
@@ -107,120 +99,100 @@ def get():
                 log.append(geoLongitude)
 
         if lat:
-            preference_dict['latitude']=''
-            preference_dict['longitude']=''
-            preference_dict['suggestionareaname']=''
-
             if not string==str1:
                 string=string+"&"
-            string=string+"latitude="
+            string=string+"lat="
 
             for item in lat:
                 string=string+str(item)+","
-                preference_dict['latitude']=preference_dict['latitude']+str(item)+","
-            preference_dict['latitude']=preference_dict['latitude'][:-1]
+            string=string[:-1]+"&long="
             
-            string=string[:-1]+"&longitude="
             for item in log:
                 string=string+str(item)+","
-                preference_dict['longitude']=preference_dict['longitude']+str(item)+","
-            string=string[:-1]+"&suggestionareaname="
-            
-            preference_dict['longitude']=preference_dict['longitude'][:-1]
+            string=string[:-1]+"&areas="
+
             for item in location:
                 string=string+str(item)+"$$"
-                preference_dict['suggestionareaname']=preference_dict['suggestionareaname']+str(item)+"$$"
             string=string[:-2]
-            preference_dict['suggestionareaname']=preference_dict['suggestionareaname'][:-2]
 
         if cityid:
             if not string==str1:
                 string=string+"&"
             string=string+"cityid="+max(cityid)
-            preference_dict['cityid']=max(cityid)
 
     if apt_type:
         if not string==str1:
             string=string+"&"
         string=string+"propertytype="+apt_type[0]
-        preference_dict['propertytype']=apt_type[0]
     else:
         if not string==str1:
             string=string+"&"
         string=string+"propertytype="+"APARTMENT"
-        preference_dict['propertytype']="APARTMENT"
     
 
     if bhk:
         if not string==str1:
             string=string+"&"
         string=string+"bhk="+str(min(bhk))
-        preference_dict['bhk']=str(min(bhk))
+
     elif bhk_desc:
         for item in bhk_desc:
             if item in ["small","tiny"]:
                 if not string==str1:
                     string=string+"&"
                 string=string+"bhk=2"
-                preference_dict['bhk']="2"
                 break
             if item in ["big","huge","large"]:
                 if not string==str1:
                     string=string+"&"
                 string=string+"bhk=4"
-                preference_dict['bhk']="4"
                 break
 
     if total_budget:
         if not string==str1:
             string=string+"&"
         string=string+"budget="+str(total_budget)
-        preference_dict['budget']=total_budget
+
         
-    if amenities:
-        preference_dict['amenities']=''    
+    if amenities:   
         if not string==str1:
             string=string+"&"
         string=string+"amenityid="
         for item in amenities:
             string=string+str(item)+","
-            preference_dict['amenities']=preference_dict['amenities']+str(item)+","
         string=string[:-1]
-        preference_dict['amenities']=preference_dict['amenities'][:-1]
     if not string==str1:
             string=string+"&"
     string=string+"possession="+str(poss)+"&position=Location,Budget,Size,Possession,Amenities&limit=0,30"
-    preference_dict["position"]="Location,Budget,Size,Possession,Amenities"
-    preference_dict["limit"]= "0,30"
+
     print string
-    preference.append(preference_dict)
-    print preference
-    stringformationtime = time.time()
-    
-    results = requests.get(string, params = {},headers={"token_id":token_id})
+    endtime = time.time()
+    print "total Time " , endtime - starttime
     try :
-        result =  results.json()
+        url = urlopen(string).read()
+        result = json.loads(url)
+        return {
+		"result": result,
+		"url": string,
+		}
 
     except:
-         result = {
-"data": [],
-"msg": "zero projects", 
-"status": 0, 
-"total": 0,
-}    
-    jtime = time.time()
-#    print "NLP time " , nlptime-starttime
-#    print  "stringForm  " , stringformationtime - nlptime
-#    print "jtime  " ,  jtime - stringformationtime
-    print "total Time " , jtime - starttime
-    result['preference'] = preference
-    result['url'] = string
-    return jsonify(result)
-
+        result =  {
+		"data": [], 
+		"msg": "zero projects", 
+		"status": 0, 
+		"total": 0
+		}
+        return {
+		"result": result,
+		"url": string,
+		}
+    #return url 
+    #return {"text":todo_id,"string":string,"apt_type":apt_type,"BHK":bhk,"Budget":total_budget,"Amenities":amenities,"Location":location,"Possession":possession}
+    #return {"text":todo_id,"apt_type":apt_type,"BHK":bhk,"Budget":total_budget,"Amenities":amenities,"Location":location,"latitude":geoLatitude,"longitude":geoLongitude}
 
 
 if __name__ == '__main__':
 #    app.run(host='0.0.0.0',port=6020)
-    http_server = WSGIServer(('0.0.0.0', 6030), app)
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
     http_server.serve_forever()
-
