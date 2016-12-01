4ee864477fee8f2bf43bdbc173f01fca4ad58187
Prasoon Kumar <prasoon.kumar@hdfcred.com>
2016-12-01 17:34:13 +0530

    changes

--- desktopapi.py
+++ desktopapi.py
@@ -46,16 +46,16 @@ def get():
                     if  possession >=36:
                         poss=6
                 else:
-                if possession <6:
-                    poss=2
-                if  possession >=6 and possession <12:
-                    poss=3
-                if  possession >=12 and possession<24:
-                    poss=4
-                if  possession >=24 and possession<36:
-                    poss=5
-                if  possession >=36:
-                    poss=6
+                    if possession <6:
+                        poss=2
+                    if  possession >=6 and possession <12:
+                        poss=3
+                    if  possession >=12 and possession<24:
+                        poss=4
+                    if  possession >=24 and possession<36:
+                        poss=5
+                    if  possession >=36:
+                        poss=6
 
             except:
                 if possession <6:
